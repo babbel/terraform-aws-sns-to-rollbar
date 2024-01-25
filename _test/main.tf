5ab2a5d7c10e4a8428615c029f01c7ef3ed9d599
Jan Sebastian Siwy <github@jansiwy.de>
2024-01-25 20:32:33 +0100

    Allow skipping of the JSON parsing

--- _test/main.tf
+++ _test/main.tf
@@ -2,12 +2,30 @@ provider "aws" {
   region = "local"
 }
 
-module "sns-to-rollbar" {
+module "sns-to-rollbar-with-json-key" {
   source = "./.."
 
   name = "example"
 
   json_key = "message"
+
+  rollbar_project_access_token = {
+    access_token = "some-token"
+  }
+
+  environment = "test"
+  level       = "debug"
+
+  tags = {
+    app = "some-service"
+    env = "test"
+  }
+}
+
+module "sns-to-rollbar-without-json-key" {
+  source = "./.."
+
+  name = "example"
 
   rollbar_project_access_token = {
     access_token = "some-token"
